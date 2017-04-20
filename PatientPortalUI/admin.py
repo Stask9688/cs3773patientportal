70fa30e690c714e71dce0884579349b4ad650783
bucknercd <men839@my.utsa.edu>
2017-04-20 18:09:42 -0500

    added Vital class and registered it

--- PatientPortalUI/admin.py
+++ PatientPortalUI/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import PatientRecord, Medication, Visit
+from .models import PatientRecord, Medication, Visit, Vital
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ from .models import PatientRecord, Medication, Visit
 admin.site.register(PatientRecord)
 admin.site.register(Medication)
 admin.site.register(Visit)
+admin.site.register(Vital)
--- PatientPortalUI/models.py
+++ PatientPortalUI/models.py
@@ -13,7 +13,12 @@ class PatientRecord(models.Model):
 class Medication(models.Model):
     medication_name = models.CharField(max_length=200, default="Ibuprofen")
     record = models.ForeignKey(PatientRecord, default=None)
-
+class Vital(models.Model):
+    height = models.CharField(max_length=200, default="5'7")
+    temp  = models.DecimalField(max_digits=5, decimal_places=1, default=98.7)
+    weight = models.DecimalField(max_digits=5, decimal_places=1, default=180) 
+    systolic = models.DecimalField(max_digits=3, decimal_places=0, default=120)
+    diastolic = models.DecimalField(max_digits=3, decimal_places=0, default=80)
 
 # Basic model for storing patient visit data
 class Visit(models.Model):

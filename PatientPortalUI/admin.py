889fac80ef52a7dacf79976dbcc822c374a418b7
stask9688 <stask9688@yahoo.com>
2017-04-20 20:29:10 -0500

    Added Profile model, added date and user association to Vital, modified Admin.py so data tables show more relevant fields

--- PatientPortalUI/admin.py
+++ PatientPortalUI/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from .models import PatientRecord, Medication, Visit, Vital
+from .models import PatientRecord, Medication, Visit, Vital, Profile
+
 
 # Register your models here.
+class ProfileDetail(admin.ModelAdmin):
+    list_display = ("user", "birth_date", "street_line1", "zipcode", "city", "state")
+
+
+class MedicationDetail(admin.ModelAdmin):
+    list_display = ("medication_name",)
+
+
+class VitalDetail(admin.ModelAdmin):
+    list_display = ("date", "user", "height", "weight", "systolic", "diastolic")
+
 
 # Register the model we made in models.py with the Django
 # admin
 admin.site.register(PatientRecord)
-admin.site.register(Medication)
+admin.site.register(Medication, MedicationDetail)
 admin.site.register(Visit)
-admin.site.register(Vital)
+admin.site.register(Vital, VitalDetail)
+admin.site.register(Profile, ProfileDetail)
--- PatientPortalUI/models.py
+++ PatientPortalUI/models.py
@@ -1,24 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ssn = models.CharField(max_length=11, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    street_line1 = models.CharField(max_length=100, blank=True)
+    street_line2 = models.CharField(max_length=100, blank=True)
+    zipcode = models.CharField(max_length=5, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
 
 # Basic model for storing patient records
 class PatientRecord(models.Model):
     user = models.OneToOneField(User, default=None)
 
+    def __str__(self):
+        return self.user.username
+
 
 # Basic model for storing medication data
 class Medication(models.Model):
-    medication_name = models.CharField(max_length=200, default="Ibuprofen")
-    record = models.ForeignKey(PatientRecord, default=None)
+    medication_name = models.CharField(max_length=30, blank=True, default=None)
+    description = models.TextField(max_length=400, null=True, default='')
+
+
 class Vital(models.Model):
-    height = models.CharField(max_length=200, default="5'7")
-    temp  = models.DecimalField(max_digits=5, decimal_places=1, default=98.7)
-    weight = models.DecimalField(max_digits=5, decimal_places=1, default=180) 
+    date = models.DateField(blank=True, default=date.today)
+    user = models.ForeignKey(User, blank=False, default=None)
+    height = models.CharField(max_length=200, default="5'7\"")
+    temp = models.DecimalField(max_digits=5, decimal_places=1, default=98.7)
+    weight = models.DecimalField(max_digits=5, decimal_places=1, default=180)
     systolic = models.DecimalField(max_digits=3, decimal_places=0, default=120)
     diastolic = models.DecimalField(max_digits=3, decimal_places=0, default=80)
+
 
 # Basic model for storing patient visit data
 class Visit(models.Model):
